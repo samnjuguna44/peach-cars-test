9303751836c9feca4dc12e65b426ee8e119ad122
Samson Njuguna <96062644+samnjuguna44@users.noreply.github.com>
2023-05-15 10:06:07 +0300

    creating Login component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<!-- <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +25,4 @@ nav {
     }
   }
 }
-</style>
+</style> -->
